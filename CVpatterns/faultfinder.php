c8f65a7d6dc3376a588eba1ab79960d38d224311
drdhaval2785 <drdhaval2785@gmail.com>
2014-10-04 17:59:07 +0530

    data

--- CVpatterns/faultfinder.php
+++ CVpatterns/faultfinder.php
@@ -1,133 +1,184 @@
-﻿﻿<?php
+﻿<?php
 /* set execution time to an hour */
 ini_set('max_execution_time', 36000);
 /* set memory limit to 1000 MB */
 ini_set("memory_limit","1000M");
 include "C:\\xampp\\htdocs\\sanskrit\\dev-slp.php";
 include "C:\\xampp\\htdocs\\sanskrit\\function.php";
-include "C:\\xampp\\htdocs\\sanskrit\\mystyle.css";
-$file=file('c:\\devanagari.txt');
-$file = array_map("convert1",$file);
+$file=file('PWKslp.txt');
+//$file = array_map("convert1",$file);
 $hlplus = array_merge($hl,array("M","H"));
 
 /* VV pattern */
-echo "The following are VV patterns which have to be checked for correctness :";
-foreach ($file as $value)
+/*foreach ($file as $value)
 {
     if(preg_match('/([aAiIuUfFxXeEoO][aAiIuUfFxXeEoO])/',$value))
     {
-//        echo $value." - ";
+        $vvwords[] = $value; 
         $vvraw = preg_split('/([aAiIuUfFxXeEoO][aAiIuUfFxXeEoO])/',$value,null,PREG_SPLIT_DELIM_CAPTURE);
-        $vv = $vvraw[1];
-        echo $vv."<br>";
+        $vv[] = $vvraw[1];        
     }
 }
+$vv=  array_unique($vv);
+$vv=  array_values($vv);
+$vv = sort($vv);
+*/
+
 /* VCV pattern */
-/*foreach ($file as $value)
+/*$vcv=array();
+foreach ($file as $value)
 {
     if(preg_match('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value))
     {
-        echo $value." - ";
-        $vvwords[] = $value; 
-        $vvraw = preg_split('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value,null,PREG_SPLIT_DELIM_CAPTURE);
-        $vv = $vvraw[1];
-        echo $vv."<br>";
+        $vcvwords[] = $value; 
+        $vcvraw = preg_split('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value,null,PREG_SPLIT_DELIM_CAPTURE);
+        $i=2;
+        while($i<count($vcvraw))
+        { 
+        $vcv=array_merge($vcv,array($vcvraw[$i-1])); 
+        $i=$i+2;
+        }
     }
-}*/
+}
+$vcv=  array_unique($vcv);
+$vcv=  array_values($vcv);
+$vcv = sort($vcv);
+ */
+
 /* VCCV pattern */
-/*foreach ($file as $value)
+/*$vccv=array();
+foreach ($file as $value)
 {
     if(preg_match('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value))
     {
-//        echo $value." - ";
         $vccvwords[] = $value; 
         $vccvraw = preg_split('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value,null,PREG_SPLIT_DELIM_CAPTURE);
         $i=2;
         while($i<count($vccvraw))
         {
-            $vccv="";
-            $vccv=$vccv.$vccvraw[$i-1].",";
+            $vccv=array_merge($vccv,array($vccvraw));
             $i=$i+2;
-            $vccvarray[] = $vccv;
         }
-//        echo $vccv."<br>";
     }
 }
-$vccvarray = array_unique($vccvarray);
-$vccvarray = array_values($vccvarray);
-sort($vccvarray);
-print_r($vccvarray);
-echo count($vccvarray)."<br>";
-echo count($ac)*count($hlplus)*count($hlplus)*count($ac);
+$vccv = array_unique($vccv);
+$vccv = array_values($vccv);
+sort($vccv);
+print_r($vccv);
 */
 /* VCCCV pattern */
-/*foreach ($file as $value)
+/*$vcccv=array();
+foreach ($file as $value)
 {
     if(preg_match('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value))
     {
-//        echo $value." - ";
         $vcccvwords[] = $value; 
         $vcccvraw = preg_split('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value,null,PREG_SPLIT_DELIM_CAPTURE);
         $i=2;
         while($i<count($vcccvraw))
         {
-            $vcccv="";
-            $vcccv=$vcccv.$vcccvraw[$i-1].",";
+            $vcccv=array_merge($vcccv,array($vcccvraw[$i-1]));
             $i=$i+2;
-            $vcccvarray[] = $vcccv;
         }
-//        echo $vcccv."<br>";
     }
 }
-$vcccvarray = array_unique($vcccvarray);
-$vcccvarray = array_values($vcccvarray);
-sort($vcccvarray);
-print_r($vcccvarray);
-echo count($vcccvarray)."<br>";
-echo count($ac)*count($hlplus)*count($hlplus)*count($hlplus)*count($ac);
-*/
+$vcccv = array_unique($vcccv);
+$vcccv = array_values($vcccv);
+sort($vcccv);
+print_r($vcccv);*/
+
 /* VCCCCV pattern */
-$invccccv=array( [0] => ANktra, [1] => ANktya, [2] => ANkzya, [3] => Andrya, [4] => Antrya, [5] => ArSvya, [6] => ArdDya, [7] => ArkzRu, [8] => ArkzyA, [9] => Arkzya, [10] => Artrya, [11] => Artsna, [12] => Artvya, [13] => ArzRya, [14] => Arzwya, [15] => Astvya, [16] => AtsTya, [17] => EkzRya, [18] => IkzRya, [19] => Okzmya, [20] => Ukzmya, [21] => Urkzya, [22] => aNktyu, [23] => akzmyA, [24] => antrya, [25] => apsnya, [26] => arkzya, [27] => artsya, [28] => arttrI, [29] => attrya, [30] => azwrya, [31] => ettrya, );
-echo "The following are VV patterns which have to be checked for correctness :";
+function vccccv($a)
+{
+$file= file($a);
+$vccccv=array();
 foreach ($file as $value)
 {
     if(preg_match('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value))
     {
+        $vccccvwords[] = $value; 
         $vccccvraw = preg_split('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value,null,PREG_SPLIT_DELIM_CAPTURE);
-        if(count(array_diff($vccccvraw,$invccccv))>0 && count($vccccvraw)>1)
+        $i=2;
+        while($i<count($vccccvraw))
         {
-            echo $value."<br>";
+            $vccccv=array_merge($vccccv,array($vccccvraw[$i-1]));
+            $i=$i+2;
         }
-        
+    }
+}
+$vccccv = array_unique($vccccv);
+$vccccv = array_values($vccccv);
+sort($vccccv);
+return $vccccv;
+}
+
+$input = vccccv("PWKslp.txt");
+print_r($input);
+/* testing in MW */
+$file1=file("MWdev.txt");
+$file1=array_map('convert1',$file1);
+foreach ($file1 as $value)
+{
+    if(preg_match('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value))
+    {
+        $vccccvex = preg_split('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value,null,PREG_SPLIT_DELIM_CAPTURE);        
+        $i=2;
+        while ($i<count($vccccvex))
+        {
+            if ( !in_array($vccccvex[$i-1],$input ))
+            {
+                echo "<p class=red>".$value." - ".$vccccvex[$i-1]."</p><br>";
+            }
+            else
+            {
+                echo $value." - ".$vccccvex[$i-1]."<br>";
+            }
+            $i=$i+2;
+        }
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* VCCCCCV pattern */
-/*foreach ($file as $value)
+/*$vcccccv=array();
+foreach ($file as $value)
 {
     if(preg_match('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value))
     {
-//        echo $value." - ";
         $vcccccvwords[] = $value; 
         $vcccccvraw = preg_split('/([aAiIuUfFxXeEoO][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][kKgGNcCjJYwWqQRtTdDnpPbBmyrlvzSs][aAiIuUfFxXeEoO])/',$value,null,PREG_SPLIT_DELIM_CAPTURE);
         $i=2;
         while($i<count($vcccccvraw))
         {
-            $vcccccv="";
-            $vcccccv=$vcccccv.$vcccccvraw[$i-1].",";
+            $vcccccv=array_merge($vcccccv,array($vcccccvraw[$i-1]));
             $i=$i+2;
-            $vcccccvarray[] = $vcccccv;
         }
 //        echo $vcccccv."<br>";
     }
 }
-$vcccccvarray = array_unique($vcccccvarray);
-$vcccccvarray = array_values($vcccccvarray);
-sort($vcccccvarray);
-print_r($vcccccvarray);
-echo count($vcccccvarray)."<br>";
-echo count($ac)*count($hlplus)*count($hlplus)*count($hlplus)*count($hlplus)*count($hlplus)*count($ac);
+$vcccccv = array_unique($vcccccv);
+$vcccccv = array_values($vcccccv);
+print_r($vcccccv);
 */
+
+// Pending from here.
 
 /* start-CC pattern */
 /*foreach ($file as $value)
