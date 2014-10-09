1ddebd6a4dd479363567d1f1c3cbdb5ee478cacb
Marcis Gasuns <gasyoun@users.noreply.github.com>
2014-10-09 23:38:21 +0400

    Create conf.php
    http://stackoverflow.com/questions/1712973/how-to-include-config-php-efficiently

---
+++ conf.php
@@ -0,0 +1,9 @@
+<?
+/* Configuration of the Sorting Script */
+$test = file("C:\devanagari.txt");
+$test = array_map('convert',$test);
+//$test = file("C:\\Users\\Dhaval\\Desktop\\devanagari.txt");
+
+$fileopen=file("c:\\devanagarisorted.txt");
+$outfile2=fopen("C://devanagarisorted1.html","w+");
+?>
